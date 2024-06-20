e82448cc51c791c3c83968ff1da992823bdf2b4d
04BR4H4M <88157378+04BR4H4M@users.noreply.github.com>
2024-06-19 19:54:21 -0500

    Update connection.php

--- Crud_php_mysql/connection.php
+++ Crud_php_mysql/connection.php
@@ -1,15 +1,34 @@
 <?php
-function connection(){
-    $host = "localhost";
-    $user = "root";
-    $pass ="";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "tic_nilo";
 
-    $bd ="mydb";
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    $connect = mysqli_connect($host, $user, $pass);
-
-    mysqli_select_db($connect, $bd);
-
-    return $connect;
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
 }
+
+// Obtener datos del formulario
+$nombre = $_POST['nombre'];
+$cedula = $_POST['cedula'];
+$dependencia = $_POST['dependencia'];
+$telefono = $_POST['telefono'];
+$correo = $_POST['correo'];
+$mensaje = $_POST['mensaje'];
+
+// Insertar datos en la base de datos
+$sql = "INSERT INTO registro_incidentes (nombre, cedula, dependencia, telefono, correo, mensaje)
+VALUES ('$nombre', '$cedula', '$dependencia', '$telefono', '$correo', '$mensaje')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Nuevo registro creado exitosamente";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
